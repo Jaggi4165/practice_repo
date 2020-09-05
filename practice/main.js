db8c50a075bbc91993ca6b001ac33c6931e949e9
jaggi4165 <kusumuru4165@gmail.com>
2020-09-05 09:53:22 +0530

    (XMLHttp and Fetch and promices)

--- practice/main.js
+++ practice/main.js
@@ -1,9 +1,36 @@
-var details={
-	name:"apssdc",
-	age:5,
-	courses:["html","java","php","python"]
+// var details={
+// 	name:"apssdc",
+// 	age:5,
+// 	courses:["html","java","php","python"]
+// }
+// for (var i = 0; i < details.courses.length; i++) {
+// 	document.write(details.courses[i]);
+// 	document.write("<br>")
+// }
+
+// function addd(a,b){
+// 	document.write(a+b);
+// }
+// addd(6,8)
+// document.write("<br>");
+// addd=(a,b)=>{
+// 	document.write(a+b);
+
+// }
+// addd(10,20)
+
+
+var a=10
+{
+	var a=20
+	document.write(a,"<br>")
 }
-for (var i = 0; i < details.courses.length; i++) {
-	document.write(details.courses[i]);
-	document.write("<br>")
+document.write(a,"<br>")
+
+
+let b=10;
+{
+	let b=20;
+	document.write(b,"<br>");
 }
+document.write(b);
